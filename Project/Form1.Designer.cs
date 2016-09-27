aa91c1126d8feef658e7fccc1acaaf739e843368
Christopher Ganda <Christopher Ganda>
2016-09-28 00:18:12 +0700

    ubah dtp dengan scroll

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dtpTanggal = new System.Windows.Forms.DateTimePicker();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vsbMax = new System.Windows.Forms.VScrollBar();
+            this.vsbMin = new System.Windows.Forms.VScrollBar();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(683, 328);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dtpTanggal);
+            this.tabPage1.Controls.Add(this.lblMax);
+            this.tabPage1.Controls.Add(this.lblMin);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.vsbMax);
+            this.tabPage1.Controls.Add(this.vsbMin);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(675, 302);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dtpTanggal
+            // 
+            this.dtpTanggal.Location = new System.Drawing.Point(89, 117);
+            this.dtpTanggal.Name = "dtpTanggal";
+            this.dtpTanggal.Size = new System.Drawing.Size(200, 20);
+            this.dtpTanggal.TabIndex = 6;
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(286, 191);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(47, 33);
+            this.lblMax.TabIndex = 5;
+            this.lblMax.Text = "10";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(58, 191);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(31, 33);
+            this.lblMin.TabIndex = 4;
+            this.lblMin.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Maiandra GD", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Maximum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Minimum";
+            // 
+            // vsbMax
+            // 
+            this.vsbMax.LargeChange = 1;
+            this.vsbMax.Location = new System.Drawing.Point(292, 56);
+            this.vsbMax.Maximum = 10;
+            this.vsbMax.Name = "vsbMax";
+            this.vsbMax.Size = new System.Drawing.Size(22, 125);
+            this.vsbMax.TabIndex = 1;
+            this.vsbMax.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vsbMax_Scroll);
+            // 
+            // vsbMin
+            // 
+            this.vsbMin.LargeChange = 1;
+            this.vsbMin.Location = new System.Drawing.Point(64, 56);
+            this.vsbMin.Maximum = 10;
+            this.vsbMin.Name = "vsbMin";
+            this.vsbMin.Size = new System.Drawing.Size(22, 125);
+            this.vsbMin.TabIndex = 0;
+            this.vsbMin.Value = 10;
+            this.vsbMin.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vsbMin_Scroll);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(675, 302);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 352);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.VScrollBar vsbMax;
+        private System.Windows.Forms.VScrollBar vsbMin;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpTanggal;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static DateTime now = DateTime.Now;
+        private void vsbMin_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblMin.Text = (vsbMin.Maximum-vsbMin.Value).ToString();
+            if (vsbMax.Value > vsbMin.Value)
+            {
+                vsbMax.Value = vsbMin.Value-1;
+                lblMax.Text = (vsbMax.Maximum - vsbMax.Value).ToString();
+            }
+            dtpTanggal.MaxDate = now.AddYears(vsbMin.Value - vsbMin.Maximum);
+            dtpTanggal.MinDate = now.AddYears(vsbMax.Value - vsbMax.Maximum);
+        }
+
+        private void vsbMax_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblMax.Text = (vsbMax.Maximum - vsbMax.Value).ToString();
+            //lblMax.Text = vsbMax.Value.ToString();
+            if (vsbMax.Value > vsbMin.Value)
+            {
+                vsbMin.Value = vsbMax.Value;
+                lblMin.Text = (vsbMin.Maximum - vsbMin.Value).ToString();
+            }
+            dtpTanggal.MaxDate = now.AddYears(vsbMin.Value - vsbMin.Maximum);
+            dtpTanggal.MinDate = now.AddYears(vsbMax.Value - vsbMax.Maximum);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpTanggal.MaxDate = now.AddYears(vsbMin.Value - vsbMin.Maximum);
+            dtpTanggal.MinDate = now.AddYears(vsbMax.Value - vsbMax.Maximum);
+        }
+    }
+}
