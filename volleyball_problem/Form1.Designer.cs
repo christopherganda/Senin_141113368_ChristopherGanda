71e7779f346bf8cc3d10327890f658018ce8288c
christopher.ganda <christopher.ganda@mikroskil.ac.id>
2016-11-24 14:00:36 +0700

    volley prob

---
+++ volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(127, 39);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(127, 73);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Enabled = false;
+            this.txtHasil.Location = new System.Drawing.Point(127, 115);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(100, 20);
+            this.txtHasil.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hasil";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 298);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ volleyball_problem/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        long mod = 1000000007;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private long gcd(long a, long b)
+        {
+            if (b == 0) return a;
+            return gcd(b, a % b);
+        }
+        private long modularInverse(long value)
+        {
+            long tmp, x, y, times, modulus;
+            x = 0;
+            y = 1;
+            modulus = mod;
+            if (mod <= 1 || gcd(value, mod) != 1) return -1;
+            while (value > 1)
+            {
+                times = value / mod;
+                tmp = mod;
+                mod = value % mod;
+                value = tmp;
+                tmp = x;
+                x = y - times * x;
+                y = tmp;
+            }
+            mod = modulus;
+            return (modulus + y) % modulus;
+        }
+        private void swap(ref long a, ref long b)
+        {
+            if (a > b)
+            {
+                long temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+        private long faktorial(long a)
+        {
+            long hasil = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                hasil *= i;
+                hasil %= mod;
+            }
+            return hasil;
+        }
+        private long nCk(long a, long b)
+        {
+            long hasil = 1;
+            //MessageBox.Show(a.ToString() + " " + b.ToString());
+            hasil *= faktorial(a);
+            hasil %= mod;
+            hasil *= modularInverse(faktorial(a - b));
+            //MessageBox.Show(mod.ToString());
+            hasil %= mod;
+            hasil *= modularInverse(faktorial(b));
+            hasil %= mod;
+            return hasil;
+        }
+        private long pangkat( long a)
+        {
+            long hasil = 1;
+            for(long i = 1;i<=a;i++){
+                hasil*=2;
+                hasil %= mod;
+            }
+            return hasil;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long a = Convert.ToInt64(txtA.Text);
+            long b = Convert.ToInt64(txtB.Text);
+            swap(ref a,ref  b);
+            //MessageBox.Show(a.ToString() + " " + b.ToString());
+            if (b < 25 || b - a < 2 || (b > 25 && b - a != 2))
+            {
+                txtHasil.Text  = "0";
+            }
+            else
+            {
+                long hasil = nCk(Math.Min(a + b - 1, 47), Math.Min(b - 1, 24));
+                //MessageBox.Show(hasil.ToString());
+                //MessageBox.Show(Math.Min(a + b - 1, 47).ToString() + " " + Math.Min(b - 1, 24).ToString());
+                //MessageBox.Show(Math.Min(b - 1, 24).ToString());
+                hasil *= pangkat(b - 25);
+                //MessageBox.Show(pangkat(b-25,2).ToString());
+                hasil %= mod;
+                txtHasil.Text = hasil.ToString();
+               
+            }
+        }
+    }
+}
